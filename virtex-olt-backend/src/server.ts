216c0a81f8a756acc0bb7a4de1993ec9d0e111a7
devrenangabriel <devrenangabriel@gmail.com>
2025-10-15 08:13:37 -0300

    feat: adiciona rotas para manipulação dos dados de OLT

--- virtex-olt-backend/src/server.ts
+++ virtex-olt-backend/src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma.js";
 import { saveOltOnInit } from "./save-olt-data-on-init.js";
+import type { OltData } from "./generated/prisma/index.js";
 
 const HOST = process.env.HOST ?? "0.0.0.0";
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3333;
@@ -14,8 +15,79 @@ app.get("/", () => {
 });
 
 // TODO: Para sistemas simples não é necessário dividir o código em camadas (controller, service, use cases, ...), adicionar caso o sistema cresça.
+app.post("/olt-data", async (request, reply) => {
+  const body = request.body as OltData[];
+
+  if (!body || body.length === 0) {
+    return reply.status(400).send({ error: "Body é obrigatório" });
+  }
+
+  try {
+    await prisma.oltData.createMany({
+      data: body,
+      skipDuplicates: true,
+    });
+
+    return reply.status(201).send({ message: "Dados inseridos com sucesso" });
+  } catch (error) {
+    console.error(error);
+    return reply.status(400).send({ error: "Erro ao inserir dados" });
+  }
+});
+
 app.get("/olt-data", async () => {
   return prisma.oltData.findMany();
+});
+
+app.patch("/olt-data", async (request, reply) => {
+  const body = request.body as (Partial<OltData> & { id: number })[];
+
+  if (!body || body.length === 0) {
+    return reply.status(400).send({ error: "Body é obrigatório" });
+  }
+
+  try {
+    await Promise.all(
+      body.map((data) => {
+        const { id, ...rest } = data;
+
+        const oltDataExists = prisma.oltData.findUnique({ where: { id } });
+
+        // TODO: Saltamos a atualização se o registro não existir, deveriamos retornar um erro?
+        if (!oltDataExists) {
+          return Promise.resolve();
+        }
+
+        return prisma.oltData.update({ where: { id }, data: rest });
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return reply.status(400).send({ error: "Erro ao atualizar dados" });
+  }
+});
+
+app.delete("/olt-data", async (request, reply) => {
+  const body = request.body as { ids: number[] };
+
+  if (!body || body.ids.length === 0) {
+    return reply.status(400).send({ error: "Body é obrigatório" });
+  }
+
+  try {
+    await prisma.oltData.deleteMany({
+      where: {
+        id: {
+          in: body.ids,
+        },
+      },
+    });
+
+    return reply.status(200).send({ message: "Dados deletados com sucesso" });
+  } catch (error) {
+    console.error(error);
+    return reply.status(400).send({ error: "Erro ao deletar dados" });
+  }
 });
 
 app.listen({ host: HOST, port: PORT }).then((url) => {
